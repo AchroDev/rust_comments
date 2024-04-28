e4af2ff1e2d4a928edee0ec5f25f579b18e8c312
AchroDev <ztblaine@protonmail.com>
2024-04-27 21:07:01 -0500

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+// This is a simple comment
+
+// This is a simple
+// multi-line comment
+
+/*
+This is the preferred multi-line comment
+*/
+
+/*
+*   We can make multi-line comments look
+*   better by writing it like this.
+*/
+
+// You may see comments written out to the side like below:
+fn main() {
+    let lucky_number = 7; // I'm feeling lucky today
+}
+
+// However, more often, you will see comments written above the code like so:
+fn main2() {
+    // I'm feeling lucky today
+    let lucky_number = 7;
+}
